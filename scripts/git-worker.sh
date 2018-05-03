3d5cd560a98b67af8371e346a3c4be14c6f0282d
Erich Malfertheiner <erichmalfertheiner01@gmail.com>
2018-05-03 02:30:05 -0300

    update

--- scripts/git-worker.sh
+++ scripts/git-worker.sh
@@ -6,9 +6,9 @@ echo 'git-worker v0.0.1'
 sleep 1
 echo 'running uploader'
 sleep 1
-sh 'gitfiles\gituploader.sh'
+sh 'c:\users\erich\desktop\home\erich\spaces\develop\utilities\scripts\gitfiles\gituploader.sh'
 sleep 1
 echo 'running loader'
 sleep 1
-sh 'gitfiles\gitloader.sh'
+sh 'c:\users\erich\desktop\home\erich\spaces\develop\utilities\scripts\gitfiles\gitloader.sh'
 sleep 1
