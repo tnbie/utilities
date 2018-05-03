81e3c3767c310ec88a587503e3c1bdc081c6c64a
Erich Malfertheiner <erichmalfertheiner01@gmail.com>
2018-05-03 19:14:13 -0300

    update query

--- database-samples/query-model.sql
+++ database-samples/query-model.sql
@@ -1,2 +1,2 @@
 SELECT * FROM product;
-SELECT product_id, name, categorie, price FROM product WHERE product_id = 2 AND PRICE >= 2.00;
+SELECT product_id, name, price FROM product WHERE categorie = fruit AND PRICE >= 2.00;
