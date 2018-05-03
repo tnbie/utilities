9c047b103f7894598d6168581121bae3d3c9b98b
Erich Malfertheiner <erichmalfertheiner01@gmail.com>
2018-05-03 02:41:06 -0300

    upload files

--- scripts/README.md
+++ scripts/README.md
@@ -1,1 +1,5 @@
 
+# Boilerplate Example 
+- Generate via shell script. 
+- Created by [@thenewbiemaster](https://github.com/thenewbiemaster) 
+- Please change this text. 
--- scripts/executor.sh
+++ scripts/executor.sh
@@ -1,7 +1,23 @@
 #!/bin/bash +x
 
 #!/bin/bash utf-8
-echo 
+
+
+echo 'executor v0.0.1'
+sleep 1
+echo 'start!'
+sleep 1
+echo 'start creating files...' 
 sh file_creator.sh
-sleep 5
+sleep 1
+echo 'finished...'
+sleep 1
+echo 'boilerplating!'
+sleep 1
+sh 'boilerfiles/boilerplate.sh'
+sleep 1
+echo 'finished...'
+echo 'finishing'
+sleep 1
 exit
+
