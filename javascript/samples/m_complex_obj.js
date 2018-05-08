8a109b43b5cfa1bd4256f8a0a59c0e357927315a
Erich Malfertheiner <erichmalfertheiner01@gmail.com>
2018-05-08 18:55:59 -0300

    create file

---
+++ javascript/samples/m_complex_obj.js
@@ -0,0 +1,23 @@
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CS", 
+      "8T", 
+      "LP" ],
+    "gold": true
+  },
+   {
+    "artist": "Eminem",
+    "title": "Rap God",
+    "release_year": 2016,
+    "formats": [ 
+      "CS", 
+      "8T", 
+      "LP" ],
+    "gold": true
+  } 
+];
