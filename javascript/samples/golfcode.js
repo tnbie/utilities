66dc963cae9b436b59a5aba7a2cfb337a815b9da
Erich Malfertheiner <erichmalfertheiner01@gmail.com>
2018-05-08 15:57:12 -0300

    create file

---
+++ javascript/samples/golfcode.js
@@ -0,0 +1,23 @@
+// Code Snippet
+// Golf Code
+function golfScore(par, strokes) {
+  // Only change code below this line
+if (strokes == 1 ){
+  return "Hole-in-one!";
+} else if (strokes <= par - 2){
+  return "Eagle";
+} else if (strokes == par - 1){
+  return "Birdie";
+} else if (strokes == par){
+  return "Par";
+  } else if (strokes == par + 1){
+  return "Bogey";
+ } else if (strokes == par + 2){
+  return "Double Bogey";
+} else {
+  return "Go Home!";
+}
+}
+
+// Change these values to test
+golfScore(1, 1);
