800a21a2d1c1694805c50dcd197f7a5e4cb7632a
Erich Malfertheiner <erichmalfertheiner01@gmail.com>
2018-05-08 21:20:45 -0300

    create file

---
+++ javascript/OOP/unique_obj_construc.js
@@ -0,0 +1,10 @@
+
+var Car = function(wheels, seats, engines) {
+  //Change this constructor
+  this.wheels = wheels;
+  this.seats = seats;
+  this.engines = engines;
+};
+
+//Try it out here
+var myCar = new Car(4, 5, 1);
