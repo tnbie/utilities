5716fb85b7e0bddb6da9291ca515ffc134b5eb2b
Erich Malfertheiner <erichmalfertheiner01@gmail.com>
2018-12-10 01:15:16 -0200

    upload file

--- git_upload_commit.bat
+++ git_upload_commit.bat
@@ -9,10 +9,6 @@ set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
 set "fullstamp=%YYYY%-%MM%-%DD% %HH%-%Min%-%Sec%"
-:: echo datestamp: "%datestamp%"
-:: echo timestamp: "%timestamp%"
-:: echo fullstamp: "%fullstamp%"
-
 
 :: view status before
 echo [TIME] %fullstamp% :preview changes
