55d4d626812418142ce3b7c4a1364e397b62dbbd
Erich Malfertheiner <erichmalfertheiner01@gmail.com>
2018-12-10 01:25:28 -0200

    upload file

--- git_upload_commit.bat
+++ git_upload_commit.bat
@@ -11,42 +11,50 @@ set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
 set "fullstamp=%YYYY%-%MM%-%DD% %HH%-%Min%-%Sec%"
 
 :: view status before
-color 06 echo [INFO] %fullstamp% :preview changes
+color 06 
+echo [INFO] %fullstamp% :preview changes
 @echo off 
 git status
 @echo off
 
 :: add files for change
-color 07 echo [INFO] %fullstamp% :adding files
+color 07 
+echo [INFO] %fullstamp% :adding files
 @echo off
 git add .
 @echo off
 
 :: get status after
-color 05 echo [INFO] %fullstamp% :preloaded changes
+color 05 
+echo [INFO] %fullstamp% :preloaded changes
 @echo off
 git status
 @echo off
 
 :: commit changes for file 
-color 03 echo [INFO] %fullstamp% :commit changes
+color 03 
+echo [INFO] %fullstamp% :commit changes
 @echo off
 git commit -m "upload file"
 @echo off
 
 :: upload changes to remote
-color 02 echo [INFO] %fullstamp% :uploading started
+color 02 
+echo [INFO] %fullstamp% :uploading started
 @echo off
 git push origin master
-color 04 echo [INFO] %fullstamp% :uploading finished
+color 04 
+echo [INFO] %fullstamp% :uploading finished
 @echo off
 
 :: pause file execution
-color 01 echo [INFO] %fullstamp% :pausing
+color 01 
+echo [INFO] %fullstamp% :pausing
 @echo off
 :: pause
 
 :: exit from file
-color 0C echo [INFO] %fullstamp% :exiting
+color 0C 
+echo [INFO] %fullstamp% :exiting
 @echo off
 :: exit
