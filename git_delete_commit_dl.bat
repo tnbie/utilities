29cb91771552f732ecec3110956f71d391ef5e30
Erich Malfertheiner <erichmalfertheiner01@gmail.com>
2018-12-10 01:52:09 -0200

    upload file

--- git_delete_commit_dl.bat
+++ git_delete_commit_dl.bat
@@ -1,24 +1,58 @@
-:: template for delete duplicated file
+:: template for delete duplicate file
 :: git vcs
 :: author @erichm
 
-:: set echo off
+:: create dates
 @echo off
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set "fullstamp=%YYYY%-%MM%-%DD% %HH%-%Min%-%Sec%"
 
 :: view status before
+echo [INFO]-%fullstamp% :preview changes
+@echo off
 git status
+@echo off
 
 :: add files for change
+echo [INFO]-%fullstamp% :adding files
+@echo off
 git add .
+@echo off
 
-:: get status
+:: get status after
+echo [INFO]-%fullstamp% :preloaded changes
+@echo off
 git status
+@echo off
 
 :: commit changes for file 
-git commit -m "delete file due duplication"
+echo [INFO]-%fullstamp% :commit changes
+@echo off
+git commit -m "delete duplicate file" 
+@echo off
 
 :: upload changes to remote
+echo [INFO]-%fullstamp% :upload started
+@echo off
 git push origin master
+echo [INFO]-%fullstamp% :upload finished
+@echo off
+
+:: get status
+echo [INFO]-%fullstamp% :status now
+@echo off
+git status
+@echo off
+
+:: pause file execution
+echo [INFO]-%fullstamp% :pausing
+@echo off
+::pause
 
 :: exit from file
+echo [INFO]-%fullstamp% :exiting
+@echo off
 exit
