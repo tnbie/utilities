4d121be2d4721f99b1a4bc57a747ef34e7f10ef4
Erich Malfertheiner <erichmalfertheiner01@gmail.com>
2018-12-10 01:44:25 -0200

    upload file

--- java/dataModel.java
+++ java/dataModel.java
@@ -1,10 +1,13 @@
 public class dataModel{
 public static void main (String args []){
     // declare value of the variable
+	String pon = "";
+	String activitie = "";
+	String valueOf = "The value is: ";
     int x = 9;
     int y = 89;
     int z = x * y;
-    String valueOf = "The value is: ";
+
     // put business logic here
     // print the value of y
     System.out.println(valueOf + " " + y);
