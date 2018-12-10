faee3562f42afdda3546392d984589614219a14b
Erich Malfertheiner <erichmalfertheiner01@gmail.com>
2018-12-10 01:05:36 -0200

    upload file

--- git_upload_commit.bat
+++ git_upload_commit.bat
@@ -1,14 +1,24 @@
 :: template for upload file
 :: git vcs
 :: author @erichm
-date echo getting status
-date echo getting status
 
 :: set echo off
 @echo off
 
+@echo off
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+echo datestamp: "%datestamp%"
+echo timestamp: "%timestamp%"
+echo fullstamp: "%fullstamp%"
+pause
+
 :: view status before
-echo getting status
+fullstamp getting status
 git status
 
 :: add files for change
