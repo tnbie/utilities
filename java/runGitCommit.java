19f2c0d776da7354fa17559896bdd4854dee3d19
Erich Malfertheiner <erichmalfertheiner01@gmail.com>
2018-12-10 02:46:07 -0200

    upload file

--- java/runGitCommit.java
+++ java/runGitCommit.java
@@ -4,6 +4,10 @@ import java.lang.*;
 public class runGitCommit {
   public static void main(String[] argv) throws Exception {
       Runtime.getRuntime().exec("cmd /c start C:\\Users\\erichm\\Desktop\\utilities\\git_upload_commit.bat");
-      Thread.sleep(1000000);
+      Thread.sleep(1000);
+  }
+  public static void runFixCommit (String[] argv)throws Exception {
+      Runtime.getRuntime().exec("cmd /c start C:\\Users\\erichm\\Desktop\\utilities\\git_fix_commit.bat");
+      Thread.sleep(1000);
   }
 }
